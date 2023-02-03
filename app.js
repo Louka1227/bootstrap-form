ac18f76bbcaa2b0283ef7cd4ba68f7c63f34d15c
kasmi malek <kasmimalek123@gmail.com>
2023-02-03 20:24:09 +0100

    sign up btn

---
+++ app.js
@@ -0,0 +1,10 @@
+function sign() {
+  const data = {
+    fname: document.getElementById("fname").value,
+    lname: document.getElementById("lname").value,
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value,
+    confirmP: document.getElementById("confirmeP").value,
+  };
+  console.log(data);
+}
