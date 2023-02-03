b3178a78e92b840a41dc81fa249baa079c255d0e
kasmi malek <kasmimalek123@gmail.com>
2023-02-03 21:46:03 +0100

    sign up btn:bugfixe

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
+function reset() {
+  document.getElementById("fname").value = "";
+  document.getElementById("lname").value = "";
+  document.getElementById("dob").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("password").value = "";
+  document.getElementById("confirmeP").value = "";
+  document.getElementById("accept").checked = false;
+}
 function sign() {
   const data = {
     fname: document.getElementById("fname").value,
     lname: document.getElementById("lname").value,
+    birth: document.getElementById("dob").value,
     email: document.getElementById("email").value,
     password: document.getElementById("password").value,
     confirmP: document.getElementById("confirmeP").value,
+    accept: document.getElementById("accept").checked,
   };
   console.log(data);
+  reset();
 }
